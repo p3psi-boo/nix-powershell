7a306ec13cf5d3bcd8f57dd1f3e41bdff429faeb
Timofey ZAKREVSKIY <timofey.zakrevskiy@3ds.com>
2021-06-25 10:24:05 +0200

    TerminalIcons 0.5.1 -> 0.5.2

--- Terminal-Icons/default.nix
+++ Terminal-Icons/default.nix
@@ -4,8 +4,8 @@
   terminal-icons = import ./../common/fetcher.nix {
     inherit pkgs;
     name = "Terminal-Icons";
-    ver = "0.5.1";
-    sha = "0g0llcfldzyrf3pgga86fc6532270wlg53w6ji5fxzx1ws28n70d";
+    ver = "0.5.2";
+    sha = "1s1nx6pvi4lk809mx35nalwgciyjrlxmc2zbp9vl2rjw687izfaf";
   };
 
 }
